8e300e238c9984ab843d641cc1083b11c32490e9
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2026-05-18 20:38:04 +0200

    update port

--- services/proxy/index.js
+++ services/proxy/index.js
@@ -6,22 +6,22 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const authProxy = proxy("http://localhost:10010", {
+const authProxy = proxy("http://localhost:9000", {
   proxyReqPathResolver: (req) => {
     return `/api/v1/auth${req.url}`;
   },
 });
 
-const postProxy = proxy("http://localhost:10020", {
+const postProxy = proxy("http://localhost:9001", {
   proxyReqPathResolver: (req) => {
     return `/api/v1/posts${req.url}`;
   },
 });
 
-app.use("/api/v1/auth/", authProxy);
-app.use("/api/v1/posts/", postProxy);
+app.use("/api/v1/auth", authProxy);
+app.use("/api/v1/posts", postProxy);
 
-app.listen(10030, (err) => {
+app.listen(9002, (err) => {
   if (err) {
     return console.log(err);
   }
