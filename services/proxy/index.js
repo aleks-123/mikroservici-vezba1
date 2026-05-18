d31d8e5aae4b4c719317bf6e8752d55297830cda
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2026-05-18 21:27:36 +0200

    test

--- services/proxy/index.js
+++ services/proxy/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+//! npm install express-http-proxy
 const proxy = require("express-http-proxy");
 const cors = require("cors");
 
@@ -6,13 +7,28 @@ const app = express();
 app.use(cors());
 
 app.get("/health", (req, res) => {
-  res.status(200).send("OK");
+  res.status(200).send("Health okey");
 });
 
-const PORT = 9002;
-app.listen(PORT, (err) => {
+// Креирање на деве инстацни на прокси посредник
+const authProxy = proxy("http://localhost:9000", {
+  proxyReqPathResolver: (req) => {
+    return `/api/v1/auth${req.url}`;
+  },
+});
+const postProxy = proxy("http://localhost:9001", {
+  proxyReqPathResolver: (req) => {
+    return `/api/v1/posts${req.url}`;
+  },
+});
+
+//овде ќе ги употребиме миделверите што ги правевме погоре
+app.use("/api/v1/auth/", authProxy);
+app.use("/api/v1/posts/", postProxy);
+
+app.listen(9002, (err) => {
   if (err) {
     return console.log(err);
   }
-  console.log(`Proxy service started on port ${PORT}`);
+  console.log("Proxy service started on port 9002");
 });
